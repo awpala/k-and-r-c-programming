2f1fe5cf3343fd8383426ef170feb161c0cdee63
awpala <adam.w.pala@gmail.com>
2021-03-15 20:59:20 -0500

    Add ch04 reverse Polish calculator multi-file

---
+++ src/ch-04/ch-04-p082-reverse-Polish-calculator/calc.h
@@ -0,0 +1,12 @@
+#define NUMBER '0'    /* signal that a number was found */
+
+/* -- function prototypes -- */
+
+/* stack.c */
+void push(double);
+double pop(void);
+/* getop.c */
+int getop(char []);
+/* getch.c */
+int getch(void);
+void ungetch(int);
