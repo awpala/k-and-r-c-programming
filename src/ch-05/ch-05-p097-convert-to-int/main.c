04b8e0d0f315fe218970a8691be57661c6056c06
awpala <adam.w.pala@gmail.com>
2021-03-16 18:03:53 -0500

    Add ch05 convert to int multi-line

---
+++ src/ch-05/ch-05-p097-convert-to-int/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "utilities.h"
+
+#define SIZE 5
+
+/*
+  N.B. compile via gcc & bash: `$gcc -o main main.c utilities.h getch.c getint.c`, then execute `./main`. 
+*/
+
+main()
+{
+  int n, array[SIZE], getint(int *);
+
+  printf("Enter %d integers separated by white space and then hit `ENTER`:\n", SIZE);
+
+  for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
+    ;
+
+  printf("\n`getint` calls completed...\n");
+
+  return 0;
+}
---
+++ src/ch-05/ch-05-p097-convert-to-int/utilities.h
@@ -0,0 +1,12 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+/* -- function prototypes -- */
+
+/* getch.c */
+int getch(void);
+void ungetch(int);
+/* getint.c */
+int getint(int *);
+
+#endif /* UTILITIES_H */
