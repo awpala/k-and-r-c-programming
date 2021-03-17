e45ebf6f8064e7d7b9cd43b65d148dad00cc254c
awpala <adam.w.pala@gmail.com>
2021-03-17 18:35:01 -0500

    Add ch05 convert words to decl

---
+++ src/ch-05/ch-05-p126-convert-words-to-decl/utilities.h
@@ -0,0 +1,22 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+/* declarations */
+
+#define MAXTOKEN 100
+
+enum { NAME, PARENS, BRACKETS };
+
+int tokentype;              /* type of last token */
+char token[MAXTOKEN];       /* last token string */
+char out[1000];             /* output string */
+
+/* function prototypes */
+
+/* getchar.c */
+int getch(void);
+void ungetch(int);
+/* gettoken.c */
+int gettoken(void);
+
+#endif /* UTILITIES_H */
