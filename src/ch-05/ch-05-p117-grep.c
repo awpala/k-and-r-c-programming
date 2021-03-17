55610dd1abefc36c327674f1c4baebe32b5eb7cd
awpala <adam.w.pala@gmail.com>
2021-03-16 23:57:33 -0500

    Refactor from array subscript to pointer offset

--- src/ch-05/ch-05-p117-grep.c
+++ src/ch-05/ch-05-p117-grep.c
@@ -63,11 +63,11 @@ int getLine(char *line, int max)
   int c, i;
 
   for (i=0; i<max-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-    line[i] = c;
+    *(line+i) = c;
   if (c == '\n') {
-    line[i] = c;
+    *(line+i) = c;
     ++i;
   }
-  line[i] = '\0';
+  *(line+i) = '\0';
   return i;
 }
